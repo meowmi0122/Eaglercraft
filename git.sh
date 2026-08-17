e0a31e756ffcf1c41023cdb90e0d363cf9d4d64d
meowmi0122 <meowmi0122@gmail.com>
2026-08-17 02:16:30 +0000

    Update

--- git.sh
+++ git.sh
@@ -1,35 +1,3 @@
 #!/bin/bash
 
-echo "=== Git Commit ==="
-echo ""
-
-read -p "Enter Commit Message: " message
-
-if [ -z "$message" ]; then
-    echo "Commit Message can't be nothing"
-    exit 1
-fi
-
-echo ""
-echo "Upload."
-git add .
-
-echo "Commit: $message"
-git commit -m "$message"
-
-if [ $? -ne 0 ]; then
-    echo "Commit Failed"
-    exit 1
-fi
-
-echo "Uploading to GitHub..."
-git push origin main
-
-if [ $? -eq 0 ]; then
-    echo ""
-    echo "Upload Done！"
-else
-    echo ""
-    echo "Push Failed"
-    exit 1
-fi
+git add . && git commit -m "Update" && git push origin main
